efc4d61985fd882e8d20bfb4acf44de49870c7c3
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-24 03:18:51 +0600

    change in data

--- src/components/pages/SideTasks/SideTasks.js
+++ src/components/pages/SideTasks/SideTasks.js
@@ -2,14 +2,13 @@ import React from 'react';
 import LwsTaks from './LwsTaks';
 import AddNew from './AddNew';
 
-const SideTasks = ({ search, handleSearchChange }) => {
+const SideTasks = ({ search }) => {
     return (
         <div className="lg:pl-[16rem] 2xl:pl-[23rem]">
             <main className="relative z-20 max-w-3xl mx-auto rounded-lg xl:max-w-none">
                 <AddNew />
                 <LwsTaks
                     search={search}
-                    handleSearchChange={handleSearchChange}
                 />
 
             </main>
