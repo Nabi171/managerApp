4336046b6aedd882fce27288830a5601830d9cb5
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-03-25 11:09:39 +0600

    change in repo

--- src/components/pages/SideTasks/SingleLwsTask.js
+++ src/components/pages/SideTasks/SingleLwsTask.js
@@ -68,7 +68,7 @@ const SingleLwsTask = ({ task }) => {
                         />
                     </svg>
                 </button>}
-                <button
+                {status !== "completed" && <button
                     onClick={hadnleGotoEdit}
                     className="lws-edit">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -76,7 +76,7 @@ const SingleLwsTask = ({ task }) => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                     </svg>
-                </button>
+                </button>}
                 <select className="lws-status">
                     {status == "completed" &&
                         <option value="completed" selected>Completed</option>
